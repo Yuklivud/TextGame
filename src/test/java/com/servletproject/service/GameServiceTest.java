edecdde407c34c721343af32fa0d09bcd2e79f2a
Maksym Shkarbun <suprime28@yahoo.com>
2024-04-26 21:50:36 +0300

    Test all logic

--- src/test/java/com/servletproject/service/GameServiceTest.java
+++ src/test/java/com/servletproject/service/GameServiceTest.java
@@ -1,4 +1,5 @@
-import com.servletproject.service.GameService;
+package com.servletproject.service;
+
 import com.servletproject.utils.Question;
 import com.servletproject.utils.QuestionLoader;
 import jakarta.servlet.RequestDispatcher;
@@ -7,12 +8,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.internal.matchers.Any;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,7 +22,7 @@ import static org.mockito.Mockito.*;
 
 public class GameServiceTest {
 
-    private static GameService gameService;
+    private GameService gameService;
     private static Question expectedQuestion;
     @Mock
     private HttpSession session;
@@ -39,39 +37,38 @@ public class GameServiceTest {
     @BeforeEach
     public void setUp(){
         MockitoAnnotations.openMocks(this);
-        gameService = new GameService();
+        this.gameService = new GameService(loader);
+
         expectedQuestion = new Question(0, "Test question", Arrays.asList("Answer 1", "Answer 2", "Answer 3"), "Answer 2", "");
         when(request.getSession()).thenReturn(session);
     }
-    @Disabled
+
     @Test
-//    TODO: currentQuestion is null before and after. No ideas why.
     public void testCurrentQuestion_FromIndex() throws ServletException, IOException {
         List<Question> questionsList = new ArrayList<>();
-
         questionsList.add(expectedQuestion);
 
         when(loader.loadQuestions()).thenReturn(questionsList);
         when(request.getRequestDispatcher("/view/game.jsp")).thenReturn(requestDispatcher);
-
         when(session.getAttribute("currentQuestionId")).thenReturn(0);
 
-        System.out.println("Before getLogic(): " + request.getAttribute("currentQuestion"));
         gameService.getLogic(request, response);
-        System.out.println("After getLogic(): " + request.getAttribute("currentQuestion"));
 
         verify(request).setAttribute("currentQuestion", expectedQuestion);
     }
     @Test
     public void testSessionDoReturn_currentQuestionId() throws ServletException, IOException {
+        List<Question> questionsList = new ArrayList<>();
+        questionsList.add(expectedQuestion);
+
         when(request.getSession()).thenReturn(session);
         when(session.getAttribute("currentQuestionId")).thenReturn(0);
         when(request.getRequestDispatcher("/view/game.jsp")).thenReturn(requestDispatcher);
+        when(loader.loadQuestions()).thenReturn(questionsList);
 
         gameService.getLogic(request, response);
 
         verify(session).getAttribute("currentQuestionId");
-
         assertNotNull(session.getAttribute("currentQuestionId"));
         assertEquals(0, (int) session.getAttribute("currentQuestionId"));
     }
@@ -87,12 +84,38 @@ public class GameServiceTest {
         gameService.postLogic(request, response);
 
         verify(session).setAttribute(eq("currentQuestionId"), eq(1));
-
         verify(response).sendRedirect("/game");
         verify(session, never()).invalidate();
     }
     @Test
-    public void testSendRedirectToDeadPage_IfAnswerIsIncorrect(){}
+    public void testSendRedirectToDeadPage_IfAnswerIsIncorrect() throws IOException {
+        List<Question> questionsList = new ArrayList<>();
+        questionsList.add(expectedQuestion);
+
+        when(loader.loadQuestions()).thenReturn(questionsList);
+        when(session.getAttribute("currentQuestionId")).thenReturn(0);
+        when(request.getParameter("userChoice")).thenReturn("Answer 3");
+
+        gameService.postLogic(request, response);
+
+        verify(response).sendRedirect("/view/gameover.jsp");
+        verify(session).invalidate();
+    }
     @Test
-    public void testSendRedirectToWinPage_IfAnswerIsCorrectOnLastQuestion(){}
+    public void testSendRedirectToWinPage_IfAnswerIsCorrectOnLastQuestion() throws IOException {
+        List<Question> questionsList = new ArrayList<>();
+
+        for(int i = 0; i < 10; i++){
+            questionsList.add(expectedQuestion);
+        }
+
+        when(loader.loadQuestions()).thenReturn(questionsList);
+        when(session.getAttribute("currentQuestionId")).thenReturn(9);
+        when(request.getParameter("userChoice")).thenReturn("Answer 2");
+
+        gameService.postLogic(request, response);
+
+        verify(session).setAttribute(eq("currentQuestionId"), eq(0));
+        verify(response).sendRedirect("/view/win.jsp");
+    }
 }
